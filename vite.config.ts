e85bb6059ea0e0f88d657baa3ef8524e9cf54e53
= <=>
2025-03-22 01:04:03 +0200

    changing vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: mode === "production" ? "/charity-fund/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
